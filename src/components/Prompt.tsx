abc3b7c7f234cf4f6eee690b1eac54f10ecce1df
Deveshwar Jaiswal <deveshwar.jaiswal996@gmail.com>
2023-05-23 17:08:53 +0530

    fixed hooks

--- src/components/Prompt.tsx
+++ src/components/Prompt.tsx
@@ -1,22 +1,21 @@
-import {Component, useState} from "react";
+import {useState} from "react";
 
-export default class Prompt extends Component<{ onSubmit: any }> {
-    render() {
-        let {onSubmit} = this.props;
-        const [promptInput, setPromptInput] = useState("");
-        return (
-            <textarea onChange={(e) => setPromptInput(e.target.value)}
-                      onKeyDown={(e) => {
-                          if (e.key === 'Enter') {
-                              e.preventDefault();
-                              onSubmit(promptInput);
-                              setPromptInput("");
-                          }
-                      }}
-                      value={promptInput}
-                      rows={4}
-                      placeholder="Enter your prompt here"
-                      className="w-full p-2.5 text-sm text-gray-200 bg-slate-200 rounded-lg border border-gray-300"/>
-        )
-    }
+export default function Prompt(props: any) {
+
+    let onSubmit = props.onSubmit;
+    const [promptInput, setPromptInput] = useState("");
+    return (
+        <textarea onChange={(e) => setPromptInput(e.target.value)}
+                  onKeyDown={(e) => {
+                      if (e.key === 'Enter') {
+                          e.preventDefault();
+                          onSubmit(promptInput);
+                          setPromptInput("");
+                      }
+                  }}
+                  value={promptInput}
+                  rows={4}
+                  placeholder="Enter your prompt here"
+                  className="w-full p-2.5 text-sm text-black bg-slate-200 rounded-lg border border-gray-300"/>
+    )
 }
