6f7444c677dfe864c7490d6e441da72e49142b6e
Deveshwar Jaiswal <deveshwar.jaiswal996@gmail.com>
2023-05-23 18:07:36 +0530

    infinite loop

--- src/app/learning/[stack]/page.tsx
+++ src/app/learning/[stack]/page.tsx
@@ -2,21 +2,38 @@
 
 import stacks from "@/data/stacks.json";
 import Header from "@/components/Header";
-import Message from "@/components/Message";
 import Prompt from "@/components/Prompt";
+import {useEffect, useState} from "react";
+import Message from "@/components/Message";
 
 
 export default async function Page({params}: { params: { stack: string } }) {
+    const [messages, setMessages] = useState<any[]>([]);
+    const [prompt, setPrompt] = useState<string>("");
+    useEffect(() => {
+        setMessages(prev => {
+            return [...prev, {
+                text: prompt,
+                avatar: avatar,
+                author: "human",
+                id: new Date().toISOString()
+            }]
+        })
+        console.log(messages);
+    }, [prompt])
     const res = await getProps(params);
     const stack = res.props.stack;
-    const stackKey = res.props.stackKey;
+
     const avatar = 'https://thrangra.sirv.com/Avatar2.png';
-    const onSubmit = (prompt: string) => {
-        if (prompt.trim().length === 0) {
+
+    const onSubmit = (promptInput: string) => {
+        if (promptInput.trim().length === 0) {
             return;
         }
-        alert(prompt);
+        setPrompt(promptInput);
     }
+
+
     return (
         <main className="h-full pt-10 max-w-5xl mx-auto">
             <div className="flex flex-col">
@@ -24,8 +41,10 @@ export default async function Page({params}: { params: { stack: string } }) {
             </div>
             <hr className="my-4 text-white"/>
             <div className="chat flex flex-col h-96 overflow-scroll">
-                <Message idx={0} avatar={avatar} text="What is react js ?"></Message>
-                <Message idx={1} avatar="/logo-open-ai.png" text="UI library"></Message>
+                {messages.map((message, i) =>
+                    <Message avatar={message.avatar} text={message.text} key={message.id} idx={i}
+                             author={message.author}></Message>
+                )}
             </div>
             <div className="p-4 flex">
                 <Prompt onSubmit={onSubmit}></Prompt>
--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,21 +1,17 @@
 import Image from "next/image";
-import {Component} from "react";
 
-export default class Message extends Component<{ avatar: string, text: string, idx: number }> {
-    bgColor = this.props.idx % 2 === 0 ? 'bg-slate-200' : 'bg-slate-700';
-    textColor = this.props.idx % 2 === 0 ? 'text-black' : 'text-white';
-
-    render() {
-        let {avatar, text} = this.props;
-        return (
-            <div className={`flex flex-row bg-slate-100 p-4 ${this.bgColor}`}>
-                <div className="w-[30px] relative mr-4">
-                    <Image src={avatar} width={30} height={30} alt="chat-gpt"></Image>
-                </div>
-                <div className={`w-full ${this.textColor}`}>
-                    {text}
-                </div>
+export default function Message(props: any) {
+    const bgColor = props.idx % 2 === 0 ? 'bg-slate-200' : 'bg-slate-700';
+    const textColor = props.idx % 2 === 0 ? 'text-black' : 'text-white';
+    let {avatar, text} = props;
+    return (
+        <div className={`flex flex-row bg-slate-100 p-4 ${bgColor}`}>
+            <div className="w-[30px] relative mr-4">
+                <Image src={avatar} width={30} height={30} alt="chat-gpt"></Image>
             </div>
-        )
-    }
+            <div className={`w-full ${textColor}`}>
+                {text}
+            </div>
+        </div>
+    )
 }
